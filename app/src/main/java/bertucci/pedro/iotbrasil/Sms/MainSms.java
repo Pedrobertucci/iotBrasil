96fec48a5373961c8b543bc96f11cd2a73ab889b
b_ped <bertucci.dev@gmail.com>
2017-05-09 01:18:53 -0300

    Atualização 09/05 1:30

---
+++ app/src/main/java/bertucci/pedro/iotbrasil/Sms/MainSms.java
@@ -0,0 +1,15 @@
+package bertucci.pedro.iotbrasil.Sms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import bertucci.pedro.iotbrasil.R;
+
+public class MainSms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sms);
+    }
+}
--- app/src/main/java/bertucci/pedro/iotbrasil/getway/ProfileGetway.java
+++ app/src/main/java/bertucci/pedro/iotbrasil/getway/ProfileGetway.java
@@ -1,16 +1,35 @@
 package bertucci.pedro.iotbrasil.getway;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import bertucci.pedro.iotbrasil.Devices.MainDevices;
+import bertucci.pedro.iotbrasil.MainActivity;
 import bertucci.pedro.iotbrasil.R;
+import bertucci.pedro.iotbrasil.Sms.MainSms;
 
 public class ProfileGetway extends AppCompatActivity {
+
+    Button btnSms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_getway);
         setTitle("Getway 1");
+
+
+        btnSms = (Button) findViewById(R.id.btnSms);
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(ProfileGetway.this,MainSms.class);
+                startActivity(it);
+            }
+        });
     }
 }
